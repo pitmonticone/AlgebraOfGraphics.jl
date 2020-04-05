98ad25dc0a1876ff45dc2af2313d21c6c9631f7d
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-04-05 11:04:27 +0000

    build based on 64babe1

---
+++ previews/PR34/generated/internals.jl
@@ -0,0 +1,64 @@
+# # Internals
+#
+# AlgebraOfGraphics is based on *contexts*, which can be extended. Each context is then
+# associated to a named tuple `style` (used for `x`, `y` axes or attributes in the plot)
+# and a named tuple `group` used for grouping, forming a `ContextualPair`. A list of
+# contextual pairs is called a contextual map.
+#
+# ## Contexts
+#
+# In the default context, all variables in `style` are broadcasted to a common shape, and
+# each entry corresponds to a separate trace. The syntax `dims` exists to allow setting 
+# `group` variables that only vary with one of the dimensions.
+# The `group => style` pairs corresponding to each group can be accessed with `Base.pairs`:
+
+using RDatasets: dataset
+using AlgebraOfGraphics
+d = style(:Cyl, :Hwy) * group(color = :Year)
+pairs(d)
+
+# The `DataContext` is invoked with `data(df)`, where `df` respects the Tables.jl interface.
+# `DefaultContext`s can be merged onto a `DataContext` (column names are replaced by the
+# corresponding arrays).
+
+mpg = dataset("ggplot2", "mpg")
+t = data(mpg)
+pairs(t * d)
+
+# The slicing context is another example. It is invoked with `dims(I::Int...)`, and signals
+# along which dimension on the style to dims to extract series.
+
+using AlgebraOfGraphics: dims
+ctx = dims(1)
+x = rand(5, 3, 2)
+y = rand(5, 3)
+pairs(dims(1) * style(x, y) * group(color=dims(2), marker=dims(3)))
+
+# ## Combining operations using trees
+#
+# All outputs of `group`, `style`, `data`, and `dims` inherit can be combined using `+`
+# (adding a new layer), or `*` (merge information in existing layer).
+
+using AbstractPlotting, CairoMakie
+using AlgebraOfGraphics: spec
+mpg1 = copy(mpg)
+mpg1.Displ = mpg.Displ .* 0.1
+ts = (data(mpg) * spec(markersize = 5px) + data(mpg1) * spec(markersize=10px))
+sl = ts * style(:Hwy, :Displ) * group(color=:Cyl)
+
+# The result can then be plotted using the `draw` function:
+
+sl * spec(Scatter) |> draw
+AbstractPlotting.save("tree.svg", AbstractPlotting.current_scene()); nothing #hide
+
+# ![](tree.svg)
+#
+# ## Implementing a new context
+#
+# To implement a new context, one can overload:
+#
+# - `AlgebraOfGraphics.merge_group_style(c::ContextualPair{<:MyContext}, pd)`, to allow applying `group` and `style` to `MyContext`,
+#
+# - `Base.pairs(c::ContextualPair{<:MyContext})`, which must iterates `group::NamedTuple => style::NamedTuple` pairs.
+# 
+# See example implementation in the [context file](https://github.com/JuliaPlots/AlgebraOfGraphics.jl/blob/master/src/context.jl).
---
+++ previews/PR34/generated/tutorial.jl
@@ -0,0 +1,137 @@
+# # Tutorial
+#
+# Here we will see what are the basic building blocks of AlgebraOfGraphics, and how to
+# combine them to create complex plots based on datas or other style formats.
+#
+# ## Basic building blocks
+#
+# The most important functions are `group`, `style`, and `spec`.
+# `style` determines the mapping from data to plot. Its positional arguments correspond to
+# the `x`, `y` or `z` axes of the plot, whereas the keyword arguments correspond to plot
+# attributes that can vary continuously, such as `color` or `markersize`. `group`
+# determines the grouping of the style. The style is split according to the variables listed
+# in `group`, and then styled using a default palette. Finally `spec` can be used to
+# give style-independent specifications about the plot (plotting function or attributes).
+#
+# `style`, `group`, and `spec` work in various context. In the following we will explore
+# `DataContext`, which is introduced doing `data(df)` for any tabular style structure `df`.
+# In this context, `style` and `group` accept symbols and integers, which correspond to
+# columns of the data.
+#
+# ## Operations
+#
+# The outputs of `style`, `group`, `spec`, and `data` can be combined with `+` or `*`,
+# to generate a `Layers` object, which can then be plotted with a package that supports it
+# (so far MakieLayout).
+#
+# The operation `+` is used to create separate layer. `a + b` has as many layers as `la + lb`,
+# where `la` and `lb` are the number of layers in `a` and `b` respectively.
+#
+# The operation `a * b` create `la * lb` layers, where `la` and `lb` are the number of layers
+# in `a` and `b` respectively. Each layer of `a * b` contains the combined information of
+# the corresponding layer in `a` and the corresponding layer in `b`. In simple cases,
+# however, both `a` and `b` will only have one layer, and `a * b` simply combines the
+# information.
+#
+# ## Working with datas
+
+using RDatasets: dataset
+using AlgebraOfGraphics, AbstractPlotting, CairoMakie
+mpg = dataset("ggplot2", "mpg");
+cols = style(:Displ, :Hwy);
+grp = group(color = :Cyl);
+scat = spec(Scatter)
+pipeline = cols * scat
+data(mpg) * pipeline |> draw
+AbstractPlotting.save("scatter.svg", AbstractPlotting.current_scene()); nothing #hide
+
+# ![](scatter.svg)
+#
+# Now let's simply add `grp` to the pipeline to do the grouping.
+
+data(mpg) * grp * pipeline |> draw
+AbstractPlotting.save("grouped_scatter.svg", AbstractPlotting.current_scene()); nothing #hide
+
+# ![](grouped_scatter.svg)
+# Traces can be added together with `+`.
+
+using AlgebraOfGraphics: linear
+lin = spec(linear, linewidth = 3)
+pipenew = cols * (scat + lin)
+data(mpg) * pipenew |> draw
+AbstractPlotting.save("linear.svg", AbstractPlotting.current_scene()); nothing #hide
+
+# ![](linear.svg)
+# We can put grouping in the pipeline (we filter to avoid a degenerate group).
+
+filtered = data(filter(row -> row.Cyl != 5, mpg))
+filtered * grp * pipenew |> draw
+AbstractPlotting.save("grouped_linear.svg", AbstractPlotting.current_scene()); nothing #hide
+
+# ![](grouped_linear.svg)
+# This is a more complex example, where we split the scatter plot,
+# but do the linear regression with all the style.
+
+different_grouping = grp * scat + lin
+data(mpg) * cols * different_grouping |> draw
+AbstractPlotting.save("semi_grouped.svg", AbstractPlotting.current_scene()); nothing #hide
+
+# ![](semi_grouped.svg)
+#
+# Different analyses are also possible, always with the same syntax (again we filter
+# out the degenerate group):
+
+using AlgebraOfGraphics: smooth
+filtered * cols * grp * (scat + spec(smooth(span = 0.8), linewidth = 3)) |> draw
+AbstractPlotting.save("loess.svg", AbstractPlotting.current_scene()); nothing #hide
+
+# ![](loess.svg)
+# ## Layout
+#
+# Thanks to the MakieLayout package it is possible to create plots where categorical variables
+# inform the layout.
+
+iris = dataset("datasets", "iris")
+cols = style([:SepalLength, :SepalWidth], [:PetalLength :PetalWidth])
+grp = group(layout_x = dims(1), layout_y = dims(2), color = :Species)
+geom = spec(Scatter) + spec(linear, linewidth = 3)
+data(iris) * cols * grp * geom |> draw
+AbstractPlotting.save("layout.svg", AbstractPlotting.current_scene()); nothing #hide
+
+# ![](layout.svg)
+#
+# ## Non tabular style
+#
+# The framework is not specific to datas, but can be used with anything that the plotting
+# package supports.
+
+x = [-pi..0, 0..pi]
+y = [sin cos] # We use broadcasting semantics on `tuple.(x, y)`.
+style(x, y) * group(color = dims(1), linestyle = dims(2)) * spec(linewidth = 3) |> draw
+AbstractPlotting.save("functions.svg", AbstractPlotting.current_scene()); nothing #hide
+
+# ![](functions.svg)
+
+import StatsMakie
+using Distributions
+mus = 1:4
+shapes = [6, 10]
+gs = InverseGaussian.(mus, shapes')
+geom = spec(linewidth = 3)
+grp = group(color = dims(1), linestyle = dims(2))
+style(fill(0..5), gs) * grp * geom |> draw
+AbstractPlotting.save("distributions.svg", AbstractPlotting.current_scene()); nothing #hide
+
+# ![](distributions.svg)
+#
+# ## Slicing context
+#
+# The algebra of graphics logic can be easily extended to novel contexts.
+# For example, `dims` implements the "slices are series" approach.
+
+s = dims(1) * style(rand(50, 3), rand(50, 3, 2))
+grp = group(color = dims(2), layout_x = dims(3))
+s * grp * spec(Scatter) |> draw
+AbstractPlotting.save("arrays.svg", AbstractPlotting.current_scene()); nothing #hide
+
+# ![](arrays.svg)

6b82e05720ad36b50c3f4ad297f8d52b9bb345b9
Documenter.jl <documenter@juliadocs.github.io>
2021-08-16 11:57:33 +0000

    build based on 322327f

---
+++ previews/PR260/gallery/gallery/basic/lines_and_markers.jl
@@ -0,0 +1,34 @@
+using AlgebraOfGraphics, CairoMakie
+
+df = (x=rand(100), y=rand(100))
+xy = data(df) * mapping(:x, :y)
+draw(xy)
+
+x = range(-π, π, length=100)
+y = sin.(x)
+df = (; x, y)
+xy = data(df) * mapping(:x, :y)
+layer = visual(Lines)
+draw(layer * xy)
+
+x = range(-π, π, length=100)
+y = sin.(x)
+df = (; x, y)
+xy = data(df) * mapping(:x, :y)
+layers = visual(Scatter) + visual(Lines)
+draw(layers * xy)
+
+x = range(-π, π, length=100)
+y = sin.(x)
+df1 = (; x, y)
+df2 = (x=rand(10), y=rand(10))
+layers = data(df1) * visual(Lines) + data(df2) * visual(Scatter)
+draw(layers * mapping(:x, :y))
+
+df = (x=rand(100), y=rand(100), z=rand(100))
+xy = data(df) * mapping(:x, :y)
+layers = linear() + visual(Scatter) * mapping(color=:z)
+fg = draw(layers * xy)
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
+

60dd6dbd641928e1b958fd449d29dee567f0dbc9
Documenter.jl <documenter@juliadocs.github.io>
2021-10-11 09:30:59 +0000

    build based on 199a189

--- previews/PR287/gallery/gallery/layout/faceting.jl
+++ previews/PR287/gallery/gallery/layout/faceting.jl
@@ -1,16 +1,37 @@
 using AlgebraOfGraphics, CairoMakie
 
-df = (x=rand(100), y=rand(100), i=rand(["a", "b", "c"], 100), j=rand(["d", "e", "f"], 100))
-plt = data(df) * mapping(:x, :y, col=:i, row=:j)
+df = let
+	N = 100
+	x0 = rand(1:10, N)
+	i = rand(["α", "β"], N)
+	j = rand(["a", "b", "c"], N)
+
+	x = map(zip(x0, j)) do (xx, jj)
+		shift = jj == "a" ? -2.9 : jj == "c" ? 2.9 : 0.0
+		xx + shift
+	end
+
+	y = map(zip(x0, i)) do (xx, ii)
+		shift = ii == "α" ? -3.9 : 3.9
+		xx + 2 + shift + rand()
+	end
+
+	(; x, y, i, j)
+end
+
+plt = data(df) * mapping(:x, :y, row=:i, col=:j)
+
 draw(plt)
 
-draw(plt, facet = (; linkxaxes = false))
+draw(plt, facet = (; linkxaxes = :all))
+
+draw(plt, facet = (; linkxaxes = :none))
 
 df = (x=rand(100), y=rand(100), l=rand(["a", "b", "c", "d", "e"], 100))
 plt = data(df) * mapping(:x, :y, layout=:l)
 draw(plt)
 
-draw(plt, facet = (; linkxaxes = false, linkyaxes = false))
+draw(plt, facet = (; linkxaxes = :none, linkyaxes = :none))
 
 df1 = (x=rand(100), y=rand(100), i=rand(["a", "b", "c"], 100), j=rand(["d", "e", "f"], 100))
 df2 = (x=[0, 1], y=[0.5, 0.5], i=fill("a", 2), j=fill("e", 2))
